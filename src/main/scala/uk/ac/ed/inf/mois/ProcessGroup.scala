06a7000054c7f46011c90cbc254cc5185c9e271e
William Waites <wwaites@tardis.ed.ac.uk>
2014-06-29 23:04:21 +0000

    comments

--- src/main/scala/uk/ac/ed/inf/mois/ProcessGroup.scala
+++ src/main/scala/uk/ac/ed/inf/mois/ProcessGroup.scala
@@ -1,9 +1,16 @@
 package uk.ac.ed.inf.mois
 
+/*
+ * A `ProcessGroup` is a list of `Process`es and a `Scheduler`. It presents the
+ * same interface as a `Process` and so hierarchies of them can be built.
+ */
 abstract class ProcessGroup(name: String) extends Process(name) {
   var processes = List[Process]()
   val scheduler: Scheduler
 
+  /*
+   * The += operator adds a process to the group
+   */
   def +=(proc: Process) = {
     processes = proc :: processes
     for ((k,v) <- proc.state) {
@@ -15,6 +22,20 @@ abstract class ProcessGroup(name: String) extends Process(name) {
     this
   }
 
+  /*
+   * The -= operator removes a process from the group
+   */
+  def -=(proc: Process) = {
+    // TODO: needed for process migration. Keeping state
+    // coherent is important here
+    this
+  }
+
+  /*
+   * The `step` method of the `Process` interface calls the `Scheduler` on
+   * the listof processes together with the group state table and time
+   * parameters
+   */
   def step(t: Double, tau: Double) {
     scheduler(t, tau, state, processes:_*)
   }
