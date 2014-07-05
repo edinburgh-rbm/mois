8f674e84d376568e08bb089b944da9b26ea90112
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2014-07-05 01:53:46 +0100

    removed old comments in ProcessODETest

--- src/test/scala/uk/ac/ed/inf/mois/test/ProcessODETest.scala
+++ src/test/scala/uk/ac/ed/inf/mois/test/ProcessODETest.scala
@@ -9,30 +9,19 @@ import uk.ac.ed.inf.mois.Conversions._
 
 /** Directly calculated ODE system from Dominik's stuff. */
 object sampleODE extends ProcessODE("sample") {
-  // RHZ: This should look like this:
-  // val x1 = Var("ex:x1")
-  // val x2 = Var("ex:x2")
-  // and that should construct `VarH`s that get their value from
-  // the model that contains `sampleODE`.  In a way, models are like
-  // databases of `Var`s (in addition to collections of `Process`es).
   val x1 = Var(25.0, "ex:x1")
   val x2 = Var(50.0, "ex:x2")
   val x3 = Var(0.0, "ex:x3")
   d(x1)/dt := -0.3*x1 - 0.4*x2
   d(x2)/dt := -0.5*x1 - 0.8*x2
-  // XXXX this is wrong! Obviously it evaluates immediately and gives sin(0) always
-  // RHZ: Yes, it doesn't work because we only support polynomial
-  // functions on the rhs for now and sin(t) is not a polynomial.
-  // The best way to realise something is wrong is by not importing
-  // uk.ac.ed.inf.mois.Conversions._, the compiler error says it all.
-  // d(x3) := java.lang.Math.sin(t)
+  d(x3)/dt := java.lang.Math.sin(t)
 }
 
 class ODEProcessTest extends FlatSpec with Matchers {
 
   "sample ODE" should "give Dominik's expected results" in {
 
-    // For `should equal` to use approximate equality
+    // Use approximate equality in `should equal`
     val precision = 1e-4
     implicit val doubleEquality =
       TolerantNumerics.tolerantDoubleEquality(precision)
