1a42447f0b325b8038ee65c28970a17915ff2b31
William Waites <wwaites@tardis.ed.ac.uk>
2014-08-02 12:45:56 +0100

    add first cut of kick scheduler

---
+++ src/main/scala/uk/ac/ed/inf/mois/sched/KickSched.scala
@@ -0,0 +1,126 @@
+/*
+ *  MOIS: Kick Scheduler
+ *  Copyright (C) 2014 University of Edinburgh School of Informatics
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ed.inf.mois.sched
+
+import scala.collection.mutable
+
+import uk.ac.ed.inf.mois.{BaseProcess, ProcessGroup, Scheduler, Math}
+import uk.ac.ed.inf.mois.{DoubleVar, VarConversions, VarMap, VarMeta}
+
+class KickScheduler(
+  val tolerance: Double = 1e-1,
+  val rho: Double = 0.9,
+  val dt_min: Double = 1e-8,
+  val dt_max: Double = 1e0,
+  val threshold: Double = 1e-4)
+    extends Scheduler with Math with VarConversions {
+
+  def apply(t: Double, tau: Double, group: ProcessGroup) = {
+    val dt = if (tau > dt_max) dt_max else tau
+
+//    println("")
+//    println(s"KickSched $t + $dt")
+
+//    println(s"Drifting...")
+    // 1. allow processes to drift for the whole time
+    val x_tau = mutable.Map.empty[BaseProcess, VarMap[Double, DoubleVar]]
+    for (child <- group.processes) {
+      group >>> child
+      child.step(t, dt)
+      x_tau += child -> child.doubleVars.copy
+    }
+
+//    println(s"Calculating partials")
+    // 2. approximate partial derivatives of child at t + tau/2
+    val partials = mutable.Map.empty[BaseProcess, mutable.Map[VarMeta, VarMap[Double, DoubleVar]]]
+    for (child <- group.processes) {
+      partials += child -> child.partialDerivatives(t, dt) ///2)
+    }
+
+//    println("Drifting for half the time")
+    // 3. integrate and let drift for half the time
+    for (child <- group.processes) {
+      group >>> child
+      child.step(t, dt/2)
+    }
+
+//    println("Kicking")
+    // 4. apply the kick
+    for (child <- group.processes) {
+      val partial_c = partials(child)
+//      println(child)
+//      for (v <- partial_c.keys.toList) {
+//        val pp = partial_c(v).values.toList.sortBy(_.meta)
+//        println(s"\td/d${v.identifier} = $pp")
+//      }
+      val kick = child.doubleVars.zeros
+      for (other <- group.processes if other != child) {
+        for (x <- x_tau(other).values) {
+          for (v <- child.doubleVars.values if v != x) {
+            kick(v) += partial_c(x)(v) * x / 4 //2
+          }
+        }
+      }
+//      println(child)
+//      for (v <- child.doubleVars.values.toList.sortBy(_.meta)) {
+//        val kv = kick(v)
+//        println(s"\t$v\t$kv")
+//      }
+      child.doubleVars +:= kick
+    }
+
+//    println("Drifting the rest of the time")
+    // 5. integrate and let drift for the rest of the time
+    val x0 = group.doubleVars.copy // all variables of the group
+    val dx = group.doubleVars.zeros // accumulate the change
+    for (child <- group.processes) {
+      child.step(t+dt/2, dt/2)
+      dx +:= (child.doubleVars - x0)
+    }
+
+//    println("Calculating error")
+    // use absolute error for variables near 0 and relative for others
+    def estimateError(v: DoubleVar) = {
+      val x0_i = abs(x0(v.meta))
+      val dx_i = abs(dx(v.meta))
+      if (x0_i > threshold) // relative error
+        dx_i/x0_i
+      else
+        dx_i
+    }
+    val err = x0.values.map(estimateError(_)).max
+//    println(x0.values.toList.sortBy(_.meta))
+//    println(dx.values.toList.sortBy(_.meta))
+//    println(s"error: $err")
+
+    val new_dt = max(dt_min, min(dt_max, rho * dt * tolerance / err))
+
+    if (err < tolerance) {
+      // all good, update group variables
+//      println("all good")
+      group.doubleVars <<< (x0 + dx)
+      (t+dt, new_dt)
+    } else {
+//      println("oh oh, re-do, timestep too big")
+      // error too large, try again. attention tail recursion!
+      if (dt == new_dt)
+        throw new Exception(s"Minimum time step reached. err = ${err}")
+      apply(t, new_dt, group)
+    }
+  }
+}
---
+++ src/test/scala/uk/ac/ed/inf/mois/test/KickTest.scala
@@ -0,0 +1,66 @@
+/*
+ *  MOIS: Kick Scheduler Test
+ *  Copyright (C) 2014 University of Edinburgh School of Informatics
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ed.inf.mois.test
+
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalactic.TolerantNumerics
+
+import uk.ac.ed.inf.mois.{Model, ProcessGroup, VarContainer, VarConversions, VarMeta}
+import uk.ac.ed.inf.mois.sched.{KickScheduler}
+
+class KickedModel extends Model {
+  val w = Double("w") := -1.0
+  val k = Double("k") := 0.5
+  val process = new ProcessGroup("Coupled Oscillator Group") {
+    scheduler = new KickScheduler(tolerance=0.1)
+  }
+  process += new CoupledOscillatorA(w, k)
+  process += new CoupledOscillatorB(w, k)
+}
+
+/** Run the two versions of the system of ODEs with the NaiveScheduler. */
+class KickSchedulerTest extends FlatSpec with Matchers with VarConversions {
+
+  // Use approximate equality in `should equal`
+  val precision = 1e-4
+  implicit val doubleEquality =
+    TolerantNumerics.tolerantDoubleEquality(precision)
+
+  import scala.math.abs
+  implicit def stringToMeta(s: String) = VarMeta(s)
+
+  def maxerr(p1: VarContainer, p2: VarContainer) =
+    Seq("x1", "x2", "x3", "x4")
+      .map(v => abs(1-p1.doubleVars("c:" + v)/p2.doubleVars("d:" + v)))
+      .max
+
+  "coupled oscillator" should "give similar results directly as with weisse" in {
+    val direct = new CoupledOscillatorModel
+    val group = new KickedModel
+
+    direct.init(0)
+    direct.run(0, 1)
+    direct.finish
+
+    group.init(0)
+    group.run(0, 1)
+    group.finish
+
+    (maxerr(direct.process, group.process) < 0.035) should  be (true)
+  }
+}
